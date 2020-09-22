6c4f4dfaa43abd9112de6a773f4d029ac72cc490
Anders West Rasmussen <anders.west.rasmus@gmail.com>
2020-09-22 14:16:51 -0500

    Update classification.py

--- decryptr/classify/classification.py
+++ decryptr/classify/classification.py
@@ -24,7 +24,7 @@ def classify_procedure(effect_file, target_file, convolution_matrix, logfilename
         print("decryptr: using default hyperparameters (two-state model)")
 
     if parallelize in ['True', 'true']:
-	parallelize = True
+        parallelize = True
         print("decryptr: using parallelized implementation")
         print("please note this will discard distant off target binding events")
     else:
