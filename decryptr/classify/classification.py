c75a9ca59f5aad54f8b36c3a2fdbafa38a6f6a2b
Anders West Rasmussen <anders.west.rasmus@gmail.com>
2020-09-22 16:49:19 -0500

    Update classification.py

--- decryptr/classify/classification.py
+++ decryptr/classify/classification.py
@@ -230,7 +230,7 @@ def classify_procedure(effect_file, target_file, convolution_matrix, logfilename
 
                 terminal_idx = terminal_idxs[i]
                 print(np.shape(target_locs <= terminal_idx))
-                print(target_locs <= terminal_idx).flatten())
+                print((target_locs <= terminal_idx).flatten())
 		
                 terminal_guide_idx = np.argmax((target_locs <= terminal_idx).flatten()) + 1
 
