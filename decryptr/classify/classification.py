8b2718d47a6c3432f7104fc9eb6daefdedd32b67
Anders West Rasmussen <anders.west.rasmus@gmail.com>
2020-09-22 18:44:53 -0500

    Update classification.py

--- decryptr/classify/classification.py
+++ decryptr/classify/classification.py
@@ -363,7 +363,7 @@ def run_slice(cmat, convolved_signal, x, target_locs, alpha_opt, rho_opt, sn_opt
     mean_f, var_f, x_truncated = gp_deconvolution.pred([cmat], [convolved_signal], [np.asarray(x, dtype=int)],
                                                        [target_locs], alpha_opt,
                                                        rho_opt, sn_opt,
-                                                       [1 / prec_vec], full_pred=True)
+                                                       [1 / prec_vec], full_pred=False)
 
     x_vals = np.asarray(x[np.argwhere(x_truncated[0] == True)].flatten(), dtype=int)
 
